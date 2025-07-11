b4dfdac03398506c8312e757ed4689f84af5fb81
zpjayyy <jay.lichzp@gmail.com>
2025-07-11 12:53:42 +0800

    useing pem save private key

--- src/process/text.rs
+++ src/process/text.rs
@@ -1,7 +1,8 @@
 use std::fs;
 
 use anyhow::Result;
-use ed25519_dalek::SigningKey;
+use ed25519_dalek::pkcs8::EncodePrivateKey;
+use ed25519_dalek::{SigningKey, pkcs8::spki::der::pem::LineEnding};
 use rand::{TryRngCore, rngs::OsRng};
 
 use crate::GenerateKeyOpts;
@@ -9,7 +10,13 @@ use crate::GenerateKeyOpts;
 pub fn generate_key(opts: GenerateKeyOpts) -> Result<SigningKey> {
     let mut csprng = OsRng.unwrap_err();
     let signing_key = SigningKey::generate(&mut csprng);
-    println!("{:?}", signing_key.to_keypair_bytes());
-    fs::write(opts.output, signing_key.to_keypair_bytes())?;
+    let pem = signing_key
+        .to_pkcs8_pem(LineEnding::default())
+        .expect("Failed to convert to PEM");
+    fs::write(opts.output, pem)?;
     Ok(signing_key)
 }
+
+// trait Sign {}
+
+// trait Verify {}
